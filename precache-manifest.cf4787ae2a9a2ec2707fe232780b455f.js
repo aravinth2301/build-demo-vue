196a0ddbfa8af03b6382868dc7f170436d257bb8
aravinthsivarasa <aravinth2301@gmail.com>
2020-04-23 15:44:45 +1000

    first commit

---
+++ precache-manifest.cf4787ae2a9a2ec2707fe232780b455f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0172f2f625899229d009",
+    "url": "/css/chunk-vendors.cd76d981.css"
+  },
+  {
+    "revision": "09f521319ecdcfcdbf8043d19dcadb5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd55b5cd2ddfb531e9b2",
+    "url": "/js/app.3b1c0c35.js"
+  },
+  {
+    "revision": "0172f2f625899229d009",
+    "url": "/js/chunk-vendors.b340dcc5.js"
+  },
+  {
+    "revision": "ddfa7d33ad677afdf301",
+    "url": "/js/dashboard.53c42e7e.js"
+  },
+  {
+    "revision": "49f9143313c82c088f8e",
+    "url": "/js/login.1dbc4e6e.js"
+  },
+  {
+    "revision": "095d2cb3d004a73933ba",
+    "url": "/js/search data.088a6994.js"
+  },
+  {
+    "revision": "27b1ab3fec5c4c6e5b08437fcc4becdc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
